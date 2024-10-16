52f04fbff56eb044c120c42c619991b8505700e5
Abhijith P <87142203+abhijithp327@users.noreply.github.com>
2024-10-16 17:47:37 +0530

    Update Signup.jsx

--- frontend/src/components/auth/Signup.jsx
+++ frontend/src/components/auth/Signup.jsx
@@ -41,11 +41,8 @@ const Signup = () => {
     };
 
     const submitHandler = async (e) => {
-
         e.preventDefault();
-
         const formData = new FormData();
-
         formData.append("fullname", input.fullname);
         formData.append("email", input.email);
         formData.append("phoneNumber", input.phoneNumber);
